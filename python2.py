5e70198220e5ad82a082b3d650c5ad0a519e45be
by-maicl <sorokinaksusa6@gmail.com>
2026-03-27 13:52:58 +0200

    add python2.py

---
+++ python2.py
@@ -0,0 +1,13 @@
+
+stack = []
+word = input("Введіть слово: ")
+
+for letter in word:
+    stack.append(letter)
+
+reversed_word = ""
+
+while len(stack) > 0:
+    reversed_word += stack.pop()
+
+print("Перевернуте слово:", reversed_word)
