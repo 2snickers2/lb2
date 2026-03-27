593264e9a1f2e4d248ed4dfc6b68c3203cc78c55
2snickers2 <93041229+2snickers2@users.noreply.github.com>
2026-03-27 14:05:37 +0200

    Add letter counting feature to word reversal
    Added a letter counter to track the number of letters in the input word.

--- python2.py
+++ python2.py
@@ -1,13 +1,18 @@
-
 stack = []
 word = input("Введіть слово: ")
 
+# лічильник букв
+count = 0
+
 for letter in word:
     stack.append(letter)
+    count += 1   # рахуємо букви
 
 reversed_word = ""
 
 while len(stack) > 0:
     reversed_word += stack.pop()
+
 print("----------------------------------")
 print("Перевернуте слово:", reversed_word)
+print("Кількість букв:", count)
